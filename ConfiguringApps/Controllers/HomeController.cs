d141b873d70e91bd807005a05dc527a9bad27b75
Alex Grinchuck <alex.grinchuck@gmail.com>
2020-08-19 15:30:46 +0300

    Add bower and bootstrap, configurate app to use mvc, add service UptimeService and use it in HomeController

--- ConfiguringApps/Controllers/HomeController.cs
+++ ConfiguringApps/Controllers/HomeController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ConfiguringApps.Infrastructure;
 
 namespace ConfiguringApps.Controllers
 {
     public class HomeController : Controller
     {
+        private UptimeService uptime;
+        public HomeController(UptimeService up) => uptime = up;
         public ViewResult Index()
         {
-            return View(new Dictionary<string, string> {["Message"]= "This is the index action"});
+            return View(new Dictionary<string, string> {["Message"]= "This is the index action", ["Uptime"]=$"{uptime.Uptime}ms"});
         }
     }
 }
