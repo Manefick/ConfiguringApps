1f15d9baf88eb23f82f40c9ab23fe52db54aea2a
Alex Grinchuck <alex.grinchuck@gmail.com>
2020-08-22 14:24:23 +0300

    Add servises and for class of middleware

---
+++ ConfiguringApps/Infrastructure/ContentMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace ConfiguringApps.Infrastructure
+{
+    public class ContentMiddleware
+    {
+        private RequestDelegate nextDelegate;
+        private UptimeService uptime;
+        public ContentMiddleware(RequestDelegate next, UptimeService up)
+        {
+            uptime = up;
+            nextDelegate = next;
+        }
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Path.ToString().ToLower() == "/middleware")
+            {
+                await httpContext.Response.WriteAsync("This is from the content middleware"+$"  uptime:{uptime.Uptime}ms", Encoding.UTF8);
+            }
+            else
+            {
+                await nextDelegate.Invoke(httpContext);
+            }
+        }
+    }
+}
